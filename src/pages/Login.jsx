daf1c668c5e0e40ff1182ba8a30aca0599abac00
juantobares4 <j.tobares@itecriocuarto.org.ar>
2023-11-15 01:26:49 -0300

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
 function Login({ user, login, loggedIn }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
