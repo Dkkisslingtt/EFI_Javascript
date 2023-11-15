05f10d39000729a8c18c50e4119fdf96b51cfeab
dkkissling <gkissling@itecriocuarto.org.ar>
2023-11-15 01:52:14 -0300

    modificando las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login';
-import Tareas from '../componet/Tareas';
+import Tareas from '../components/Tareas';
 
 
 function Home({ loggedIn, user, login }) {
